d72efc689f2823983e9c81fc248586c6abb7f8e6
kazukii478 <152988078+kazukii478@users.noreply.github.com>
2023-12-07 23:11:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "yang nanay, ha ha so funny";
+  question.innerHTML = "login aja sendiri, gua bgabtuk";
   gif.src =
     "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 });
